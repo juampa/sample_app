3f4351184800cdd3cd859c7419346acde5e966fb
juampa <juampa@informa.es>
2012-01-12 12:25:16 +0100

    Done with sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     if @user.save
       # Lo que hagamos si todo ok
+      sign_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
